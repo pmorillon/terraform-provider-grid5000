d1258c9fab2797761f440803613f7ce6555ad1e1
Pascal Morillon <pascal.morillon@irisa.fr>
2021-02-09 08:03:08 +0100

    Add datasource grid5000_ceph_auth

--- grid5000/provider.go
+++ grid5000/provider.go
@@ -32,6 +32,7 @@ func Provider() *schema.Provider {
 			"grid5000_site":          dataSourceGrid5000Site(),
 			"grid5000_node":          dataSourceGrid5000Node(),
 			"grid5000_vlan_nodelist": dataSourceGrid5000VlanNodelist(),
+			"grid5000_ceph_auth":     dataSourceGrid5000CephAuth(),
 		},
 		ConfigureFunc: providerConfigure,
 	}
